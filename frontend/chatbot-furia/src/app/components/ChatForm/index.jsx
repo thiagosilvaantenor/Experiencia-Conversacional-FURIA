898285d82eee25938e9e642dfb19f620527bc7d5
Thiago Antenor <thiagoantenor31@gmail.com>
2025-05-03 15:43:34 -0300

    feat: add funcionalidade de listar redes sociais no front end

--- frontend/chatbot-furia/src/app/components/ChatForm/index.jsx
+++ frontend/chatbot-furia/src/app/components/ChatForm/index.jsx
@@ -39,9 +39,6 @@ export default function ChatForm() {
     ]
   };
   
-
-
-
   //Realiza a requisição GET(/chat) para o backend para exibir o menu com as opções disponiveis do backend
   useEffect(() => {
     const fetchMenu = async () => {
@@ -67,7 +64,6 @@ export default function ChatForm() {
       setMensagens(prev => [...prev, { texto: `Opção ${valor}`, remetente: 'usuario' }]);
     }
     
-
     //Altera o estado de carregando para exibir para o usuário
     setCarregando(true);
 
@@ -83,26 +79,30 @@ export default function ChatForm() {
       body: JSON.stringify(payload),
     })
     //Recebe a resposta da requisição
-    const data = await res.json();
+    const dados = await res.json();
     
    switch(valor){
     case "1":
-      verificaLive(data);
+      verificaLive(dados);
+      setCarregando(false);
+      break;
+    case "2":
+      retornaRedesSociais(dados);
       setCarregando(false);
       break;
     default:
       // Para outras opções, simplesmente mostra a mensagem
       setCarregando(false);
-      setMensagens(prev => [...prev, { texto: data.mensagem, remetente: 'bot' }]);
+      setMensagens(prev => [...prev, { texto: dados.mensagem, remetente: 'bot' }]);
     }
     
   }
 
-  //Caso opção seja = 1
-  const verificaLive = (data) => {
+  //Caso opção tenha valor = 1
+  const verificaLive = (dados) => {
     //Recebe o json(data) e verifica se alguem está emLive
-    if (data.emLive) {
-      data.emLive.forEach(jogador => {
+    if (dados.emLive) {
+      dados.emLive.forEach(jogador => {
         setMensagens(prev => [
           ...prev,
           {
@@ -116,18 +116,30 @@ export default function ChatForm() {
       setMensagens(prev => [
         ...prev,
         {
-          texto:data.mensagem, remetente: "bot"
+          texto:dados.mensagem, remetente: "bot"
         }
       ]);
-      data.canais.forEach(canal => {
+      dados.canais.forEach(canal => {
         setMensagens( prev => [
         ...prev, {
-          texto: `Siga ${canal.jogador} na Twitch:\n ${canal.canal}`,
+          texto: `Siga ${canal.jogador} na Twitch:\n ${canal.twitch}`,
           remetente: "bot"
         }
       ]);
     });
   } 
+  }
+
+  const retornaRedesSociais = (dados) => {
+      //Recebe o json(data) e retorna as redes sociais dos jogadores
+      dados.canais.forEach(canal => {
+          setMensagens( prev => [
+            ...prev, {
+              texto: `Jogador: ${canal.jogador}\nCanal da Twitch 🎮:\n${canal.twitch}\nCanal do Youtube ▶:\n${canal.youtube}\nInstagram 📸:\n${canal.instagram}`,
+              remetente: "bot"
+            }
+          ])
+      })
   }
 
 
@@ -173,7 +185,7 @@ export default function ChatForm() {
         {mensagens.map((msg, idx) => (
           <div key={idx} className={`flex ${msg.remetente === 'bot' ? 'justify-start' : 'justify-end'}`}>
             <div className={`text-black text-stro shadow-md/30 border-black p-4 rounded-2xl max-w-sm break-words whitespace-pre-wrap
-              ${msg.remetente === 'bot' ? 'bg-blue-500' : 'bg-yellow-200'}`}>
+              ${msg.remetente === 'bot' ? 'bg-blue-800' : 'bg-yellow-200'}`}>
               {msg.texto}
            </div>
           </div>
