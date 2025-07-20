a0d0315611dd84994a1f921f1682c96faf28619f
Thiago Antenor <thiagoantenor31@gmail.com>
2025-07-19 21:39:47 -0300

    refactor: adicionado constante para URL da API do backend local ou deploy

--- frontend/chatbot-furia/src/app/lib/chat/consumoApi.jsx
+++ frontend/chatbot-furia/src/app/lib/chat/consumoApi.jsx
@@ -1,12 +1,16 @@
 //Consome api do backend para mandar um GET e buscar as opções do menu
+//Caso esteja rodando localmente, descomente a linha abaixo
+// const URL = "http://localhost:8080/chat"
+//Abaixo é usado o URL do backend que esta rodando no KOYEB
+const URL = "https://single-dixie-thiagoantenor-ce6e3e58.koyeb.app/chat"
 export const fetchMenu = async () => {
-  const res = await fetch('http://localhost:8080/chat')
+  const res = await fetch(URL)
   return res.json()
 }
 
 //Consome api do backend para mandar um POST e enviar a opção do menu selecionada
 export const postChat = async (payload) => {
-  const res = await fetch('http://localhost:8080/chat', {
+  const res = await fetch(URL, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(payload),
